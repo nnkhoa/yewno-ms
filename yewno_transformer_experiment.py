10d0a2e0980c0452fba55d6de44b60baa4120a43
nnkhoa <n.nhukhoa2209@gmail.com>
2022-03-10 16:27:08 +0100

    added new path to yewno concept dictionary, add data handler, some 1st code to encode concept

--- yewno_transformer_experiment.py
+++ yewno_transformer_experiment.py
@@ -1,19 +1,61 @@
-import sentence_transformers
+from sentence_transformers import SentenceTransformer
 import pandas as pd
-import keybert
+# import keybert
 import bertopic
 import platform
+import pickle
 from flair.embeddings import TransformerDocumentEmbeddings
 
 import numpy as np
 
 
-def load_data():
-    return
+class DataHandler:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        # self._data = None
+
+    def load_data(self):
+        with open(self.data_path, 'rb') as f:
+            data = pickle.load(f)
+        return data
+
+    def save_data(self, data):
+        with open(self.data_path, 'wb') as f:
+            pickle.dump(data, self.data_path)
 
 
-def save_data():
-    return
+# class EDFHandler(DataHandler):
+#     def load_data(self):
+#         with open(self.data_path, 'rb') as f:
+#             self._data = pickle.load(f)
+#         return self._data
+#
+#     def save_data(self, data=None):
+#         with open(self.data_path, 'wb') as f:
+#             if not data:
+#                 pickle.dump(self._data, self.data_path)
+#             else:
+#                 pickle.dump(data, self.data_path)
+#
+#     def data_setter(self, data):
+#         self._data = data
+#
+#
+# class YewnoHandler(DataHandler):
+#     def load_data(self):
+#         with open(self.data_path, 'rb') as f:
+#             self._data = pickle.load(f)
+#         return self._data
+#
+#     def save_data(self, data=None):
+#         with open(self.data_path, 'wb') as f:
+#             if not data:
+#                 pickle.dump(self._data, self.data_path)
+#             else:
+#                 pickle.dump(data, self.data_path)
+#
+#     def data_setter(self, data):
+#         self._data = data
 
 
 if __name__ == '__main__':
@@ -22,6 +64,7 @@ if __name__ == '__main__':
     FILTERED_YEWNO_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/filtered_yewno.pickle'
     PERIOD_DATA_PATH = '/Users/khoanguyen/Workspace/dataset/edf_msft/'
     CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_concept_count.pickle'
+    YEWNO_CONCEPT_DICT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_dict.pickle'
     BIGRAM_CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_bigram_concept_count.pickle'
 
     monthly_file = ['2019-07-01', '2019-08-01', '2019-09-01', '2019-10-01', '2019-11-01',
@@ -36,6 +79,24 @@ if __name__ == '__main__':
         CONCEPT_COUNT_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\yewno-edf_concept_count.pickle'
         BIGRAM_CONCEPT_COUNT_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\yewno-edf_bigram_concept_count' \
                                     '.pickle '
+        YEWNO_CONCEPT_DICT_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\yewno_concept_dict.pickle'
         PERIOD_DATA_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\MSFT\\'
 
-    print('Do shit')
+    edf_handler = DataHandler(EDF_DATA_PATH)
+    edf_df = edf_handler.load_data()
+
+    yewno_dict_handler = DataHandler(YEWNO_CONCEPT_DICT_PATH)
+    yewno_dict_df = yewno_dict_handler.load_data()
+
+    yewno_concept_handler = DataHandler(FILTERED_YEWNO_PATH)
+    yewno_concept_df = yewno_concept_handler.load_data()
+
+    sample_concept = yewno_concept_df['Concept'].head().tolist()
+
+    print(sample_concept)
+
+    concept_model = SentenceTransformer('sentence-transformers/stsb-roberta-base-v2')
+
+    embeddings = concept_model.encode(sample_concept)
+
+    print(embeddings)
