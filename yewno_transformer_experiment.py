bb1e062a375c8196548c01c1147702997f7fd29a
nnkhoa <n.nhukhoa2209@gmail.com>
2022-03-16 08:12:43 +0100

    added 2 clustering methods, plotting the cluster with PCA dim-reduction

--- yewno_transformer_experiment.py
+++ yewno_transformer_experiment.py
@@ -1,18 +1,29 @@
 from sentence_transformers import SentenceTransformer
 import pandas as pd
-# import keybert
+import keybert
 import bertopic
 import platform
 import pickle
-from flair.embeddings import TransformerDocumentEmbeddings
-
+from flair.embeddings import SentenceTransformerDocumentEmbeddings
+from flair.data import Sentence
 import numpy as np
+import os
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import silhouette_score
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import hdbscan
+
+import time
+
+from tqdm import tqdm
 
 
 class DataHandler:
     def __init__(self, data_path):
         self.data_path = data_path
-        # self._data = None
 
     def load_data(self):
         with open(self.data_path, 'rb') as f:
@@ -21,8 +32,129 @@ class DataHandler:
 
     def save_data(self, data):
         with open(self.data_path, 'wb') as f:
-            pickle.dump(data, self.data_path)
+            pickle.dump(data, f)
 
+
+def remove_noise(text):
+    if ' -- ' in text:
+        text = text[text.index(' -- '):]
+    if '. Visit' in text:
+        text = text[:text.index('. Visit')]
+    if '. visit' in text:
+        text = text[:text.index('. visit')]
+    if 'Link to Company News' in text:
+        text = text[:text.index('Link to Company News')]
+    if 'Editor: ' in text:
+        text = text[:text.index('Editor: ')]
+    while '-0-' in text:
+        text = text[:text.index('-0-')]
+    while ' SOURCE ' in text:
+        text = text[:text.index(' SOURCE ')]
+    while 'NOTE:' in text:
+        text = text[:text.index('NOTE:')]
+    while 'More information can be found' in text:
+        text = text[:text.index('More information can be found')]
+    while 'CONTACT:' in text:
+        text = text[:text.index('CONTACT:')]
+    while 'For more information' in text:
+        text = text[:text.index('For more information')]
+    while 'NOTE TO EDITORS:' in text:
+        text = text[:text.index('NOTE TO EDITORS:')]
+    while 'can be found at' in text:
+        text = text[:text.index('can be found at')]
+    while 'View source version' in text:
+        text = text[:text.index('View source version')]
+
+    return text
+
+
+def text_preprocessing(text_df, stop_word=None, writing_style=None):
+    corpus = []
+    text_list = text_df.text.to_list()
+
+    for text in text_list:
+        text = remove_noise(text)
+        corpus.append(text)
+
+    token_list = [item.split(' ') for item in corpus]
+
+    text_df['pre_process'] = corpus
+    text_df['token'] = token_list
+
+    text_df = text_df[text_df['token'].str.len() > 10]
+
+    text_df.drop(['token'], axis=1, inplace=True)
+
+    return text_df
+
+
+def plot_doc_emb(doc_emb_data, cluster_label, num_cluster, cluster_center=None):
+    defaultcolor = 'black'
+    colors_map = cm.get_cmap('viridis', num_cluster)
+    colors = colors_map(range(num_cluster))
+    axes = [0, 1]
+
+    fig, ax = plt.subplots()
+    for i in range(num_cluster):
+        xy = doc_emb_data[cluster_label == i]
+
+        if cluster_center:
+            xy = np.vstack([xy, cluster_center[i]])
+
+        # print(centroid.shape)
+        # print(xy[-1].shape)
+        two_dim = PCA(n_components=max(axes)+1).fit_transform(xy)
+        color = colors[i] if i > -1 else defaultcolor
+        ax.scatter(two_dim[:-1, axes[0]], two_dim[:-1, axes[1]], color=color, label=i, s=10)
+        ax.scatter(two_dim[-1, axes[0]], two_dim[-1, axes[1]], color=color, s=40, edgecolor='black')
+
+        annotate_text = f"center#{i}"
+        ax.annotate(text=annotate_text, xy=(two_dim[-1, axes[0]], two_dim[-1, axes[1]]), fontsize=5)
+
+    ax.legend(loc='upper right', bbox_to_anchor=(0.0, 1.0), frameon=True)
+    plt.show()
+
+
+def hdbscan_clustering(doc_emb_data):
+    start_time = time.time()
+    clustering = hdbscan.HDBSCAN().fit(doc_emb_data)
+    print('Clustering time: ', time.time() - start_time)
+    labels = clustering.labels_
+
+    num_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    num_noise = list(labels).count(-1)
+
+    cluster_cores = clustering.exemplars_
+
+    faux_centroid = [np.mean(core, axis=0) for core in cluster_cores]
+
+    print('Number of Cluster: ', num_clusters)
+    print('Number of Noise: ', num_noise)
+    print(labels)
+
+    plot_doc_emb(doc_emb_data, labels, num_clusters, faux_centroid)
+
+
+def kmean_clustering(k_start, k_end, doc_emb_data):
+    ss_max = 0
+    chosen_k = 0
+    chosen_model = None
+
+    for k in range(k_start, k_end+1):
+        start_time = time.time()
+        clustering = KMeans(n_clusters=k, init='k-means++').fit(doc_emb_data)
+        ss = silhouette_score(doc_emb_data, clustering.labels_)
+        print(f'k = {k}, clustering time: ', time.time() - start_time)
+
+        if ss > ss_max:
+            ss_max = ss
+            chosen_k = k
+            chosen_model = clustering
+            print(f'New best silhouette score = {ss}, k = {chosen_k}')
+
+    centroid = list(chosen_model.cluster_centers_)
+
+    plot_doc_emb(doc_emb_data, chosen_model.labels_, chosen_k, cluster_center=centroid)
 
 # class EDFHandler(DataHandler):
 #     def load_data(self):
@@ -65,7 +197,9 @@ if __name__ == '__main__':
     PERIOD_DATA_PATH = '/Users/khoanguyen/Workspace/dataset/edf_msft/'
     CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_concept_count.pickle'
     YEWNO_CONCEPT_DICT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_dict.pickle'
+    YEWNO_CONCEPT_EMBEDDINGS_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_emb.pickle'
     BIGRAM_CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_bigram_concept_count.pickle'
+    EDF_EMB_PATH = '/Users/khoanguyen/Workspace/dataset/edf_msft/edf_emb.pickle'
 
     monthly_file = ['2019-07-01', '2019-08-01', '2019-09-01', '2019-10-01', '2019-11-01',
                     '2019-12-01', '2020-01-01', '2020-02-01', '2020-03-01', '2020-04-01',
@@ -80,23 +214,59 @@ if __name__ == '__main__':
         BIGRAM_CONCEPT_COUNT_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\yewno-edf_bigram_concept_count' \
                                     '.pickle '
         YEWNO_CONCEPT_DICT_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\yewno_concept_dict.pickle'
+        YEWNO_CONCEPT_EMBEDDINGS_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\yewno_concept_emb.pickle'
         PERIOD_DATA_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\MSFT\\'
+        EDF_EMB_PATH = 'K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\MSFT\\edf_emb.pickle'
 
-    edf_handler = DataHandler(EDF_DATA_PATH)
-    edf_df = edf_handler.load_data()
-
+    # Getting the roberta representation for concept
+    '''    
     yewno_dict_handler = DataHandler(YEWNO_CONCEPT_DICT_PATH)
     yewno_dict_df = yewno_dict_handler.load_data()
 
     yewno_concept_handler = DataHandler(FILTERED_YEWNO_PATH)
     yewno_concept_df = yewno_concept_handler.load_data()
 
-    sample_concept = yewno_concept_df['Concept'].head().tolist()
-
-    print(sample_concept)
+    concept_list = yewno_concept_df['Concept'].tolist()
 
     concept_model = SentenceTransformer('sentence-transformers/stsb-roberta-base-v2')
 
-    embeddings = concept_model.encode(sample_concept)
+    embeddings = concept_model.encode(concept_list)
 
-    print(embeddings)
+    concept_embeddings_df = pd.DataFrame(data=embeddings, index=concept_list)
+
+    yewno_emb_handler = DataHandler(YEWNO_CONCEPT_EMBEDDINGS_PATH)
+    yewno_emb_handler.save_data(concept_embeddings_df)
+    '''
+
+    # Preprocessing data and getting the roberta representation on EDF Document
+    '''
+    document_model = SentenceTransformerDocumentEmbeddings('sentence-transformers/stsb-roberta-base-v2')
+    
+    for month in monthly_file:
+        month_data_path = PERIOD_DATA_PATH + month
+        edf_handler = DataHandler(month_data_path)
+        edf_df = edf_handler.load_data()
+
+        text_preprocessing(edf_df)
+
+        text_list = edf_df['pre_process'].tolist()
+        index_list = edf_df.index.tolist()
+
+        doc_emb_list = []
+        for text in tqdm(text_list):
+            doc_sent = Sentence(text)
+            document_model.embed(doc_sent)
+
+            doc_numpy = doc_sent.embedding.detach().numpy()
+            doc_emb_list.append(doc_numpy)
+
+        doc_emb_array = np.asarray(doc_emb_list)
+        if not os.path.isdir(PERIOD_DATA_PATH + 'embeddings'):
+            os.mkdir(PERIOD_DATA_PATH + 'embeddings')
+
+        np.savetxt(PERIOD_DATA_PATH + 'embeddings/' + month + '_no-st-text_emb.csv', doc_emb_array)
+    '''
+
+    data_np = np.genfromtxt('/Users/khoanguyen/Workspace/dataset/edf_msft/embeddings/2019-08-01_emb.csv')
+
+    kmean_clustering(k_start=5, k_end=50, doc_emb_data=data_np)
