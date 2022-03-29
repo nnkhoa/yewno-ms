a80e09626618cc290279d3eec3e7f1a3fe0ac0f2
nnkhoa <n.nhukhoa2209@gmail.com>
2022-03-29 15:19:46 +0200

    added heavy pre-processing for text, added bertopic document's keyword extraction

--- yewno_transformer_experiment.py
+++ yewno_transformer_experiment.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import pandas as pd
-# from keybert import KeyBERT
-import bertopic
+from keybert import KeyBERT
+from bertopic import BERTopic
 import platform
 import pickle
 from flair.embeddings import SentenceTransformerDocumentEmbeddings
@@ -19,7 +19,11 @@ import umap
 
 from yellowbrick.cluster import KElbowVisualizer
 
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
 import time
+import re
 
 from tqdm import tqdm
 
@@ -71,24 +75,47 @@ def remove_noise(text):
     return text
 
 
-def text_preprocessing(text_df, stop_word=None, writing_style=None):
+def text_preprocessing(text_df, stop_word=None, writing_style=None, save_column='pre_process'):
     corpus = []
     text_list = text_df.text.to_list()
+    stop_words = set(stopwords.words('english'))
 
     for text in text_list:
         text = remove_noise(text)
+
+        text = re.sub(r'[^a-zA-Z.]', ' ', text)
+        # convert to lower case
+        text = text.lower()
+        # remove tags
+        text = re.sub("&lt;/?.*?&gt;"," &lt;&gt; ", text)
+        # remove special characters and digits
+        text = re.sub(r'(\d)',' ', text)
+        # removes emails and mentions (words with @)
+        text = re.sub(r"\S*@\S*\s?", " ", text)
+        # removes URLs with http
+        text = re.sub(r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-])', ' ', text)
+        # removes URLs with www
+        # text = re.sub(r'www\S*\s?', ' ', text)
+        # Convert to list from string
+        token_list = text.split(' ')
+        # Lemmatization
+        lem = WordNetLemmatizer()
+        temp = [lem.lemmatize(word) for word in token_list if word not in stop_words]
+
+        text = " ".join(temp)
+
         corpus.append(text)
 
     token_list = [item.split(' ') for item in corpus]
 
-    text_df['pre_process'] = corpus
+    text_df[save_column] = corpus
     text_df['token'] = token_list
 
-    text_df = text_df[text_df['token'].str.len() > 10]
+    new_text_df = text_df[text_df['token'].str.len() > 10]
 
-    text_df.drop(['token'], axis=1, inplace=True)
+    new_text_df.drop(['token'], axis=1, inplace=True)
 
-    return text_df
+    return new_text_df
 
 
 def plot_doc_emb(doc_emb_data, cluster_label, num_cluster, cluster_center=None):
@@ -128,7 +155,7 @@ def plot_doc_emb(doc_emb_data, cluster_label, num_cluster, cluster_center=None):
     plt.scatter(map_data[cluster_label >= 0, 0],
                 map_data[cluster_label >= 0, 1],
                 c=cluster_label[cluster_label >= 0],
-                cmap='hsv_r', s=5)
+                cmap='hsv', s=5)
     plt.colorbar()
     plt.show()
 
@@ -155,7 +182,7 @@ def hdbscan_clustering(doc_emb_data):
     print('Number of Noise: ', num_noise)
     print(labels)
 
-    plot_doc_emb(umap_embeddings, labels, num_clusters)
+    plot_doc_emb(doc_emb_data, labels, num_clusters)
 
 
 def kmean_clustering(k_start, k_end, doc_emb_data):
@@ -187,13 +214,13 @@ def kmean_clustering_yellowbrick(k_start, k_end, doc_emb_data, month):
     visualizer = KElbowVisualizer(KMeans(random_state=0), k=(k_start, k_end)).fit(doc_emb_data)
 
     # print('Optimal k: ', visualizer.elbow_value_)
-    visualizer.show(outpath='K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\elbow_kmean_msft\\' + month + '.png')
+    # visualizer.show(outpath='K:\\Lbpam\\DG_Gestion_Quant\\GERANT\\Khoa\\Data\\elbow_kmean_msft\\' + month + '.png')
     # print('Silhouette Score: ', visualizer.k_scores_)
     optimal_k = visualizer.elbow_value_
     elbow_score = visualizer.elbow_score_
     clustering = KMeans(n_clusters=optimal_k, init='k-means++', random_state=0).fit(doc_emb_data)
 
-    visualizer.poof()
+    # visualizer.poof()
 
     print(month)
     print('Optimal k: ', optimal_k)
@@ -214,6 +241,15 @@ def keybert_extraction(corpus):
         corpus_kw_list.extend(doc_kw_list)
 
     return list(set(corpus_kw_list))
+
+
+def bertopic_extraction(corpus, save_path):
+    roberta_model = SentenceTransformerDocumentEmbeddings
+    topic_model = BERTopic(embedding_model='sentence-transformers/stsb-roberta-base-v2')
+
+    topic, prob = topic_model.fit_transform(corpus)
+
+    topic_model.save(save_path)
 
 
 # class EDFHandler(DataHandler):
@@ -257,7 +293,7 @@ if __name__ == '__main__':
     PERIOD_DATA_PATH = '/Users/khoanguyen/Workspace/dataset/edf_msft/'
     CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_concept_count.pickle'
     YEWNO_CONCEPT_DICT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_dict.pickle'
-    YEWNO_CONCEPT_EMBEDDINGS_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_context_emb.pickle'
+    YEWNO_CONCEPT_EMBEDDINGS_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno_concept_no-context_emb.pickle'
     BIGRAM_CONCEPT_COUNT_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/yewno-edf_bigram_concept_count.pickle'
     EDF_EMB_PATH = '/Users/khoanguyen/Workspace/dataset/edf_msft/embeddings/'
     KEYBERT_EMBEDDINGS_PATH = '/Users/khoanguyen/Workspace/dataset/Yewno/keybert_emb.pickle'
@@ -285,18 +321,20 @@ if __name__ == '__main__':
     yewno_dict_handler = DataHandler(YEWNO_CONCEPT_DICT_PATH)
     yewno_dict_df = yewno_dict_handler.load_data()
 
-    yewno_concept_handler = DataHandler(FILTERED_YEWNO_PATH)
+    yewno_concept_handler = DataHandler(YEWNO_DATA_PATH)
     yewno_concept_df = yewno_concept_handler.load_data()
 
     concept_list = yewno_dict_df['Concept'].tolist()
-    concept_definition = yewno_dict_df['Definition'].tolist()
+    # concept_definition = yewno_dict_df['Definition'].tolist()
+    # concept_list = yewno_concept_df['Concept'].tolist()
 
-    concept_with_context = [concept + ' ' + definition
-                            for concept, definition in zip(concept_list, concept_definition)]
+    # concept_with_context = [concept + ' ' + definition
+    #                         for concept, definition in zip(concept_list, concept_definition)]
 
     concept_model = SentenceTransformer('sentence-transformers/stsb-roberta-base-v2')
 
-    embeddings = concept_model.encode(concept_with_context)
+    # embeddings = concept_model.encode(concept_with_context)
+    embeddings = concept_model.encode(concept_list)
 
     concept_embeddings_df = pd.DataFrame(data=embeddings, index=concept_list)
 
@@ -333,6 +371,7 @@ if __name__ == '__main__':
         np.savetxt(PERIOD_DATA_PATH + 'embeddings/' + month + '_no-st-text_emb.csv', doc_emb_array)
     '''
 
+    # load yewno concept embeddings
     '''
     yewno_emb_handler = DataHandler(YEWNO_CONCEPT_EMBEDDINGS_PATH)
     yewno_emb_df = yewno_emb_handler.load_data()
@@ -343,7 +382,7 @@ if __name__ == '__main__':
 
     # clustering process
     '''
-    with open(PERIOD_DATA_PATH + 'clustering_stats_elbow_no-st-text.txt', 'w+') as f:
+    with open(PERIOD_DATA_PATH + 'clustering_stats_no-context_elbow_no-st-text.txt', 'w+') as f:
         f.write('Clustering Stats: \n')
 
     for month in monthly_file:
@@ -365,11 +404,11 @@ if __name__ == '__main__':
 
             similarity_df[i] = similarity_matrix
 
-        save_path = PERIOD_DATA_PATH + month + '_yewno_similarity_elbow.pickle'
+        save_path = PERIOD_DATA_PATH + month + '_yewno_no-context_similarity_elbow.pickle'
         month_similarity_file_handler = DataHandler(save_path)
         month_similarity_file_handler.save_data(similarity_df)
 
-        with open(PERIOD_DATA_PATH + 'clustering_stats_elbow_no-st-text.txt', 'a+') as f:
+        with open(PERIOD_DATA_PATH + 'clustering_stats_no-context_elbow_no-st-text.txt', 'a+') as f:
             f.write(month + '\n')
             f.write(f'Number of clusters: {k}\n')
             f.write(f'Silhouette Score: {sil_score}\n')
@@ -377,7 +416,7 @@ if __name__ == '__main__':
                 f.write(f'Elbow Score: {elbow_score}\n')
             f.write('\n')
 
-        with open(PERIOD_DATA_PATH + month + '_cluster_elbow-labels.txt', 'w+') as f:
+        with open(PERIOD_DATA_PATH + month + '_cluster_no-context_elbow-labels.txt', 'w+') as f:
             for label in cluster_labels:
                 f.write(f'{label}\n')
     '''
@@ -412,11 +451,64 @@ if __name__ == '__main__':
     keybert_emb_handler.save_data(kw_embeddings_df)
     '''
 
-    for month in monthly_file:
+    # '''
+    if not os.path.isdir(PERIOD_DATA_PATH + 'bertopic_models'):
+        os.mkdir(PERIOD_DATA_PATH + 'bertopic_models')
+
+    for month in tqdm(monthly_file):
+        print(month + '\n')
+        month_data_path = PERIOD_DATA_PATH + month
+        edf_handler = DataHandler(month_data_path)
+        edf_df = edf_handler.load_data()
+
+        if 'pre_process' in edf_df:
+            edf_df.drop(['pre_process'], axis=1, inplace=True)
+
+        if 'heavy_pre_process' in edf_df:
+            edf_df.drop(['heavy_pre_process'], axis=1, inplace=True)
+
+        if 'heavy_pre_process' not in edf_df:
+            text_preprocessing(edf_df, save_column='heavy_pre_process')
+            edf_handler.save_data(edf_df)
+
+        text_list = edf_df['heavy_pre_process'].tolist()
+        save_path = PERIOD_DATA_PATH + 'bertopic_models/' + month + '_heavy-pre-process' + '_bertopic-model'
+
+        bertopic_extraction(text_list, save_path=save_path)
         # similarity_df = pd.DataFrame(index=yewno_emb_df.index)
 
-        month_doc_emb_path = EDF_EMB_PATH + month + '_no-st-text_emb.csv'
-        doc_emb_data = np.genfromtxt(month_doc_emb_path)
+        # month_doc_emb_path = EDF_EMB_PATH + month + '_no-st-text_emb.csv'
+        # doc_emb_data = np.genfromtxt(month_doc_emb_path)
+        #
+        # hdbscan_clustering(doc_emb_data)
+    # '''
 
-        hdbscan_clustering(doc_emb_data)
+    # extract top concepts in each month for each type of embeddings
+    '''
+    file_prefix = ['no-context_ms',      # concept related to MS extracted from yewno
+                   'no-context',         # concept extracted from yewno's dictionary
+                   'with-context']       # concept extracted from yewno's dictionary with definition as context
 
+    for month in tqdm(monthly_file):
+        # get num_cluster in month
+        sample_results_file = PERIOD_DATA_PATH + month + '_yewno_' + file_prefix[0] + '_similarity_elbow.pickle'
+        with open(sample_results_file, 'rb') as f:
+            sample_data = pickle.load(f)
+        num_cluster = sample_data.columns.tolist()
+        del sample_data
+
+        columns = pd.MultiIndex.from_product([num_cluster, file_prefix])
+        similar_concept = pd.DataFrame(columns=columns)
+
+        for prefix in file_prefix:
+            file_path = PERIOD_DATA_PATH + month + '_yewno_' + prefix + '_similarity_elbow.pickle'
+            with open(file_path, 'rb') as f:
+                data = pickle.load(f)
+            for cluster in num_cluster:
+                top_concepts = data[cluster].sort_values(ascending=False).head(20).index.tolist()
+                similar_concept[cluster, prefix] = top_concepts
+
+        save_file = month + '_similar_concepts.pickle'
+        with open(PERIOD_DATA_PATH + save_file, 'wb') as f:
+            pickle.dump(similar_concept, f)
+    '''
